409412eefb60ab80efc39f3711e350fc5a91939a
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-12-25 22:03:45 -0800

    Add Auth.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-export default function Home() {
+import { auth } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
       <main>
-        <p>Cyber Trust</p>
+        {!session ? (
+          <div>
+            <p>Cyber Trust</p>
+            <a href="/api/auth/signin">Sign in</a>
+          </div>
+        ) : (
+          <div>
+            <a href="/api/auth/signout">Sign out</a>
+          </div>
+        )}
       </main>
     </div>
   );
