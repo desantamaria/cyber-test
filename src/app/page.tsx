3b735c405027f0f0d794a186cffce7e1202ced9c
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-12-25 22:19:51 -0800

    Add Shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const session = await auth();
@@ -6,13 +7,19 @@ export default async function Home() {
     <div>
       <main>
         {!session ? (
-          <div>
-            <p>Cyber Trust</p>
-            <a href="/api/auth/signin">Sign in</a>
+          <div className="w-screen h-screen flex flex-col gap-3 justify-center items-center">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              Cyber Trust
+            </h1>
+            <Button asChild>
+              <a href="/api/auth/signin">Sign in</a>
+            </Button>
           </div>
         ) : (
-          <div>
-            <a href="/api/auth/signout">Sign out</a>
+          <div className="w-screen h-screen flex flex-col gap-3 justify-center items-center">
+            <Button asChild>
+              <a href="/api/auth/signout">Sign out</a>
+            </Button>
           </div>
         )}
       </main>
